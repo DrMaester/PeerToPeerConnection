0154569fc5a9d80f5406fd1888985238eba9e731
siemensdaniel <siemensdaniel@outlook.de>
2020-02-19 19:07:25 +0100

    Initial commit

---
+++ PeerToPeerConnection.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PeerToPeerConnection.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
